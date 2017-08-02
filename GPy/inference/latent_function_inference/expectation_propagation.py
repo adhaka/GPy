fixed a bug in gradients and extending functionality to metadata ..

GPy/inference/latent_function_inference/expectation_propagation.py
@@ -19,11 +19,12 @@ class marginalMoments(object):
         self.sigma2_hat = np.empty(num_data,dtype=np.float64)
 
 class QuadTilted(marginalMoments):
-    def __init__(self, Y, lik):
+    def __init__(self, Y, lik, Y_metadata=None):
         self.num_data = len(Y.flatten())
         super(QuadTilted, self).__init__(self.num_data)
         self.Y = Y
         self.lik = lik
+        self.Y_metadata = Y_metadata
         # number of free parameters is equal to the number of unconstrained params of likelihood ...
         self.num_params = self.lik.size
 
@@ -32,14 +33,35 @@ class QuadTilted(marginalMoments):
         self.cavity_sigma2 = sigma2
         self.cavity_sigma = np.sqrt(sigma2)
 
+    @staticmethod
+    def dict_to_list(Y_metadata, num_data):
+        if Y_metadata is not None:
+            # Pick out the relevant metadata for Yi
+            Y_metadata_list = []
+
+            for i in range(num_data):
+                Y_metadata_i = {}
+                for key in Y_metadata.keys():
+                    Y_metadata_i[key] = Y_metadata[key][i, :]
+                Y_metadata_list.append(Y_metadata_i)
+            return Y_metadata_list
+
+
     def compute_moments(self, mu, sigma2):
         self.set_cavity(mu, sigma2)
+        self.Y_metadata_list = []
+
+        self.Y_metadata_list = self.dict_to_list(self.Y_metadata, self.num_data)
+
         if self.lik.size:
             f = partial(quad_integrate.integrate, lik=self.lik, get_derivs=True)
         else:
             f = partial(quad_integrate.integrate, lik=self.lik)
 
-        quads, numevals = zip(*map(f, self.Y, self.cavity_means, self.cavity_sigma))
+        if self.Y_metadata is not None:
+            quads, numevals = zip(*map(f, self.Y, self.cavity_means, self.cavity_sigma, self.Y_metadata_list))
+        else:
+            quads, numevals = zip(*map(f, self.Y, self.cavity_means, self.cavity_sigma))
         quads = np.vstack(quads)
 
         self.Z_hat = quads[:,0]
@@ -60,8 +82,9 @@ class QuadTilted(marginalMoments):
                                                                -2*self.cavity_means*self.mu_hat) -0.5*self.Z_hat/self.cavity_sigma2
 
         self.dmu_hat_dcav_mean = self.sigma2_hat/self.cavity_sigma2
-        self.dmu_hat_dcav_var = -0.5*self.mu_hat/self.cavity_sigma2 + 0.5*(self.moment3
-                                                                         + self.cavity_means**2*self.mu_hat -2*self.cavity_means*self.moment2)/self.cavity_sigma2
+        self.dmu_hat_dcav_var = -0.5*self.mu_hat/self.cavity_sigma2 + \
+                                0.5*(self.moment3+ self.cavity_means**2*self.mu_hat
+                                     -2*self.cavity_means*self.moment2)/self.cavity_sigma2**2 - self.mu_hat*self.dZ_dcav_var/self.Z_hat
 
 
         self.dsigma2_dcav_mean = (self.moment3 - self.mu_hat*self.moment2)/self.cavity_sigma2 -2.*self.mu_hat*self.dmu_hat_dcav_mean

GPy/inference/latent_function_inference/tvb.py
@@ -27,7 +27,7 @@ class TVB(LatentFunctionInference):
         self.Sigma,_,_,self.log_det_Sigma_inv = pdinv(self.Sigma_inv)
         self.diag_Sigma = np.diag(self.Sigma)
         self.mu = np.dot(self.Sigma, self.beta*self.Ytilde )
-        self.tilted = QuadTilted(Y, likelihood)
+        self.tilted = QuadTilted(Y, likelihood, Y_metadata)
 
         #compute cavity means, vars (all at once!)
         self.cavity_vars = 1./(1./self.diag_Sigma - self.beta)
@@ -45,6 +45,20 @@ class TVB(LatentFunctionInference):
         C = np.sum(np.log(self.tilted.Z_hat))
         log_marginal = A + B + C
         return log_marginal
+
+    def _alternate_log_marginal(self):
+        """
+        adding the term KL[q||p(f|Ytilde)] to the original lower bound given above- which makes it similar to
+        nested EP.
+
+        :return:
+        """
+        A = self.K + np.diag(1./self.beta)
+        Ai, L, Li, log_det = pdinv(A)
+        Ai_ = self.Ki - self.Ki.dot(self.Sigma).dot(self.Ki)
+        log_det =self.K_logdet + self.log_det_Sigma_inv - np.sum(np.log(self.beta))
+
+        return -0.5*self.num_data*np.log(2.*np.pi) - 0.5*log_det - 0.5*Ai.dot(self.Ytilde).dot(self.Ytilde)
 
     def _log_likelihood_grads(self):
         dcav_vars_dbeta = -(self.Sigma**2/self.diag_Sigma**2 - np.eye(self.num_data))*self.cavity_vars**2

GPy/util/quad_integrate.py
@@ -116,13 +116,14 @@ def quadgk_int(f, fmin=-np.inf, fmax=np.inf, difftol=0.1):
     integrand = quadvgk(trans_func, -1., 1., difftol, difftol)
     return integrand
 
-def integrate(Y, mu, sigma, lik, get_derivs=False):
+def integrate(Y, mu, sigma, Y_metadata_i=None, lik=None, get_derivs=False):
     fmin = -np.inf
     fmax = np.inf
     SQRT_2PI = np.sqrt(2.*np.pi)
     assert np.array(Y).size == 1
+
     def generate_integral(f):
-        a = np.exp(lik.logpdf_link(f, Y)) * np.exp(-0.5 * np.square((f - mu) / sigma)) / (
+        a = np.exp(lik.logpdf_link(f, Y, Y_metadata=Y_metadata_i)) * np.exp(-0.5 * np.square((f - mu) / sigma)) / (
                 SQRT_2PI * sigma)
         powers = np.power(f, np.arange(5)[:,None])
         pp = powers[:3]